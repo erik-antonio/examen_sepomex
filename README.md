abdea08866856c40527525982d36b6471edc8fff
Angel_Muratalla <angelgarcia80@aragon.unam.mx>
2023-04-26 10:51:17 -0600

    Eliminando sobrantes

--- README.md
+++
@@ -1,2 +0,0 @@
-# examen_sepomex
-Este repositorio se creo para la realización de una BD a partir de un archivo CSV
--- sepomex.sql
+++ sepomex.sql
@@ -7,6 +7,8 @@ CREATE TABLE estados(
     id_ets              TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
     nombre              VARCHAR(45) NOT NULL UNIQUE,
 
+    CONSTRAINT nombre_sin_numeros CHECK (nombre REGEXP '^[^0-9]+$'),
+
     PRIMARY KEY(id_ets)
 )DEFAULT CHARACTER SET utf8;
 
@@ -14,6 +16,8 @@ CREATE TABLE municipios(
     id_mns              SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
     nombre              VARCHAR(45) NOT NULL UNIQUE,
     id_ets              TINYINT UNSIGNED NOT NULL,
+
+    CONSTRAINT nombre_sin_numeros CHECK (nombre REGEXP '^[^0-9]+$'),
 
     PRIMARY KEY(id_mns),
     FOREIGN KEY(id_ets) REFERENCES estados(id_ets)
@@ -23,6 +27,8 @@ CREATE TABLE ciudades(
     id_cus              SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
     nombre              VARCHAR(45) NOT NULL UNIQUE,
 
+    CONSTRAINT nombre_sin_numeros CHECK (nombre REGEXP '^[^0-9]+$'),
+
     PRIMARY KEY(id_cus)
 )DEFAULT CHARACTER SET utf8;
 
@@ -30,15 +36,19 @@ CREATE TABLE tipos(
     id_tps              TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
     nombre              VARCHAR(45) NOT NULL UNIQUE,
 
+    CONSTRAINT nombre_sin_numeros CHECK (nombre REGEXP '^[^0-9]+$'),
+
     PRIMARY KEY(id_tps)
 )DEFAULT CHARACTER SET utf8;
 
 CREATE TABLE asentamientos(
-    cp                  VARCHAR(45) NOT NULL,
+    cp                  VARCHAR   (45) NOT NULL,
     nombre              VARCHAR(145) NOT NULL,
     id_tps              TINYINT UNSIGNED NOT NULL,
     id_mns              SMALLINT UNSIGNED NOT NULL,
     id_cus              SMALLINT UNSIGNED,
+
+    CONSTRAINT cp_sin_letras CHECK (cp REGEXP '^[0-9]+$'),           
 
     PRIMARY KEY(cp, nombre),
     FOREIGN KEY(id_tps) REFERENCES tipos(id_tps),

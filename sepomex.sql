43fc25d39ecbe2fa9a885d67ab0dd822f39b3695
Angel_Muratalla <angelgarcia80@aragon.unam.mx>
2023-04-26 11:37:21 -0600

    Constraint mayusculas

--- sepomex.sql
+++ sepomex.sql
@@ -5,17 +5,20 @@ USE sepomex;
 
 CREATE TABLE estados(
     id_ets              TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    nombre              VARCHAR(45) NOT NULL UNIQUE,
+    nombre              VARCHAR(45) BINARY NOT NULL UNIQUE,
 
     CONSTRAINT nombre_sin_numeros CHECK (nombre REGEXP '^[^0-9]+$'),
+    CONSTRAINT nombre_en_mayusculas CHECK (nombre = UPPER(nombre)),
 
     PRIMARY KEY(id_ets)
 )DEFAULT CHARACTER SET utf8;
 
 CREATE TABLE municipios(
     id_mns              SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    nombre              VARCHAR(45) NOT NULL UNIQUE,
+    nombre              VARCHAR(45) BINARY NOT NULL UNIQUE,
     id_ets              TINYINT UNSIGNED NOT NULL,
+
+    CONSTRAINT nombre_en_mayusculas CHECK (nombre = UPPER(nombre)),
 
     PRIMARY KEY(id_mns),
     FOREIGN KEY(id_ets) REFERENCES estados(id_ets)
@@ -23,28 +26,33 @@ CREATE TABLE municipios(
 
 CREATE TABLE ciudades(
     id_cus              SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    nombre              VARCHAR(45) NOT NULL UNIQUE,
+    nombre              VARCHAR(45) BINARY NOT NULL UNIQUE,
+
+    CONSTRAINT nombre_en_mayusculas CHECK (nombre = UPPER(nombre)),
 
     PRIMARY KEY(id_cus)
 )DEFAULT CHARACTER SET utf8;
 
 CREATE TABLE tipos(
     id_tps              TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    nombre              VARCHAR(45) NOT NULL UNIQUE,
+    nombre              VARCHAR(45) BINARY NOT NULL UNIQUE,
+
 
     CONSTRAINT nombre_sin_numeros CHECK (nombre REGEXP '^[^0-9]+$'),
+    CONSTRAINT nombre_en_mayusculas CHECK (nombre = UPPER(nombre)),
 
     PRIMARY KEY(id_tps)
 )DEFAULT CHARACTER SET utf8;
 
 CREATE TABLE asentamientos(
     cp                  VARCHAR   (45) NOT NULL,
-    nombre              VARCHAR(145) NOT NULL,
+    nombre              VARCHAR(145) BINARY NOT NULL,
     id_tps              TINYINT UNSIGNED NOT NULL,
     id_mns              SMALLINT UNSIGNED NOT NULL,
     id_cus              SMALLINT UNSIGNED,
 
-    CONSTRAINT cp_sin_letras CHECK (cp REGEXP '^[0-9]+$'),           
+    CONSTRAINT cp_sin_letras CHECK (cp REGEXP '^[0-9]+$'), 
+    CONSTRAINT nombre_en_mayusculas CHECK (nombre = UPPER(nombre)),          
 
     PRIMARY KEY(cp, nombre),
     FOREIGN KEY(id_tps) REFERENCES tipos(id_tps),
